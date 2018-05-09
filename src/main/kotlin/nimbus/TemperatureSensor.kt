cf17570625690119def24bec22b3609c81d34d35
kasakaid <kasakaid@outlook.com>
2018-05-10 02:01:08 +0900

    不要な条件分岐を削除

--- src/main/kotlin/nimbus/TemperatureSensor.kt
+++ src/main/kotlin/nimbus/TemperatureSensor.kt
@@ -3,16 +3,14 @@ package nimbus
 import java.util.*
 
 class TemperatureSensor : Observable(), AlarmListener {
-    override fun wakeup(current:Double, time: Long) {
+    override fun wakeup(current: Double, time: Long) {
         this.setChanged()
         this.notifyObservers(Pair(current, time))
         this.clearChanged()
     }
 
     // this should be TemperatureHiLo
-    fun add(observer: Observer) {
-        if (observer is TemperatureHiLo) {
-            this.addObserver(observer)
-        }
+    fun add(observer: TemperatureHiLo) {
+        this.addObserver(observer)
     }
 }
