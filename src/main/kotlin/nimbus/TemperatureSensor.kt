8537c972fb557df078525a98c65ca389d6e47a89
kazIda <kasakaid@outlook.com>
2018-05-09 23:26:27 +0900

    observer の変更フラグを設定する。

--- src/main/kotlin/nimbus/TemperatureSensor.kt
+++ src/main/kotlin/nimbus/TemperatureSensor.kt
@@ -6,6 +6,7 @@ class TemperatureSensor : Observable(), AlarmListener {
     override fun wakeup(current:Double, time: Long) {
         this.setChanged()
         this.notifyObservers(Pair(current, time))
+        this.clearChanged()
     }
 
     // this should be TemperatureHiLo
